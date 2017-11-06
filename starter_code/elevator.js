b94b28aed672a3eebbddf2056480d7db18d8e00b
Ernesmh <ernesmh@gmail.com>
2017-11-06 18:14:57 +0100

    first iteration

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,32 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = "up"
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+      return setInterval(() => {
+       this.update();}, 1000);
+   }
+
+  stop() {
+    setTimeout (() => {
+      if(this.requests.length === 0){
+        clearInterval(id);
+      }
+    });
+  }
+
+  update() {
+      this.log()
+   }
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+  log() {
+  console.log("direction " + this.direction + " floor" + this.floor); }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,8 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+
+let elevator = new Elevator();
+intervalId = elevator.start();
+
+elevator.stop(intervalId)
